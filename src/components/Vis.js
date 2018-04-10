fc2a1ff5210dbaea12494d072b874fa6b2659b84
BFriedman117 <binyaminf@yahoo.com>
2018-04-09 20:17:50 -0400

    improved colors

--- src/components/Vis.js
+++ src/components/Vis.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 class Test extends Component {
 
+
   componentDidMount(){
+
     const canvas = this.refs.canvas;
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     const c = canvas.getContext('2d');
+
 
     window.addEventListener('resize', function(){
       canvas.width = window.innerWidth;
@@ -24,7 +27,7 @@ class Test extends Component {
     let source = a.createMediaElementSource(this.props.audio)
     source.connect(analyser)
     analyser.connect(a.destination)
-    analyser.fftSize = 512
+    analyser.fftSize = 512 / 2
     analyser.minDecibels = -90
     analyser.maxDecibels = -1
     // analyser.smoothingTimeConstant = .5;
@@ -35,22 +38,28 @@ class Test extends Component {
     let y = canvas.height / 2 + 30
     let angle
     let peak = false
+    let radius = 250
+    let bar_x, bar_y, width, height
+
 
     function animate(){
       requestAnimationFrame(animate);
       c.clearRect(0, 0, window.innerWidth, window.innerHeight)
       analyser.getByteFrequencyData(fbc)
+
       for (let i = 5; i < fbc.length; i++){
         peak = (fbc[i] > 75)
-
         angle = (i / (fbc.length / 2)) * Math.PI
-        let bar_x = x + Math.cos(angle) * fbc.length
-        let width = canvas.width / fbc.length
-        let height = -(fbc[i])
-        let bar_y = y + Math.sin(angle) * fbc.length
-        c.fillStyle = peak ? "white" : "blue"
+
+        bar_x = x + Math.cos(angle) * radius
+        bar_y = y + Math.sin(angle) * radius
+        width = canvas.width / fbc.length
+        height = -(fbc[i])
+
+        c.fillStyle = peak ? "white" : "#5bcbff"
         c.shadowBlur = peak ? 50 : 15
         c.shadowColor = peak ? "white" : "#05f2ff"
+
         c.fillRect(bar_x, bar_y, 3, height)
       }
 
@@ -62,7 +71,7 @@ class Test extends Component {
   render (){
     return (
       <div className="canvas-container">
-        <canvas ref="canvas" background="black"/>
+        <canvas ref="canvas" />
       </div>
     )
   }
